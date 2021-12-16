e8342f16601cedf1e02a0917d613b704e2baccdf
Bruno L. Carli <brunolcarli@gmail.com>
2021-12-15 22:11:05 -0300

    ADD chat log

--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
 let logged = localStorage.getItem('logged');
 let token = localStorage.getItem('token');
 let user = localStorage.getItem('user');
+let chat_logs = [];
 
 console.log('Login status:');
 console.log(logged);
--- js/requests.js
+++ js/requests.js
@@ -14,6 +14,13 @@ function json(response) {
 
 
 function query_entities(){
+    /*
+    Request the entities (elements, objects, players, etc) currently
+    active on the game. Receives a json containing logged users and
+    their position on the map.
+        - Params: None
+        - Return: Object
+    */
     var token = localStorage.getItem('token');
     var headers = {
         "cookie": "csrftoken=9YXcKsPnJSojmIXsjvqlM7TFP0tBfiU8GwVopYDWNKHSQnEUKLnPzJdsCjSb0Cfn",
@@ -21,10 +28,11 @@ function query_entities(){
         "Authorization": `JWT ${token}`
     };
     return fetch("http://104.237.1.145:11000/graphql/", {
-    // return fetch("http://localhost:11000/graphql/", {
-    "method": "POST",
-    "headers": headers,
-    "body": "{\"query\":\"query{\\n  entities(logged:true){\\n    name\\n    logged\\n    location{\\n      x\\n      y\\n    }\\n  }\\n}\\n\"}"
+        // TODO: remove commented code
+        // return fetch("http://localhost:11000/graphql/", {
+        "method": "POST",
+        "headers": headers,
+        "body": "{\"query\":\"query{\\n  entities(logged:true){\\n    name\\n    logged\\n    location{\\n      x\\n      y\\n    }\\n  }\\n}\\n\"}"
     })
     .then(json)
     .then(data => {
@@ -37,15 +45,24 @@ function query_entities(){
 
 
 function login_mutation(username, password){
+    /*
+    Request a sign in to the game server. Receives a token to be used
+    as session validation on backend requests.
+        - Params:
+            + username: string;
+            + password: string
+        - Return: null / undefined
+    */
     return fetch("http://104.237.1.145:11000/graphql/", {
-    // return fetch("http://localhost:11000/graphql/", {
-    "method": "POST",
-    "headers": {
-        "cookie": "csrftoken=9YXcKsPnJSojmIXsjvqlM7TFP0tBfiU8GwVopYDWNKHSQnEUKLnPzJdsCjSb0Cfn",
-        "Content-Type": "application/json",
-    },
-    "body": `
-    {\"query\":\"mutation{\\n  logIn(username: \\\"${username}\\\" password: \\\"${password}\\\"){\\n    token\\n  }\\n}\\n\"}`
+        // TODO: remove commented code
+        // return fetch("http://localhost:11000/graphql/", {
+        "method": "POST",
+        "headers": {
+            "cookie": "csrftoken=9YXcKsPnJSojmIXsjvqlM7TFP0tBfiU8GwVopYDWNKHSQnEUKLnPzJdsCjSb0Cfn",
+            "Content-Type": "application/json",
+        },
+        "body": `
+        {\"query\":\"mutation{\\n  logIn(username: \\\"${username}\\\" password: \\\"${password}\\\"){\\n    token\\n  }\\n}\\n\"}`
     })
     .then(json)
     .then(data => {
@@ -61,6 +78,14 @@ function login_mutation(username, password){
 
 
 function update_position(player, x, y){
+    /*
+    Updates player position on the map.
+        - Params:
+            + player: string;
+            + x: int;
+            + y: int;
+        - Return: null | undefined
+    */
     var token = localStorage.getItem('token');
     var headers = {
         "cookie": "csrftoken=9YXcKsPnJSojmIXsjvqlM7TFP0tBfiU8GwVopYDWNKHSQnEUKLnPzJdsCjSb0Cfn",
@@ -68,10 +93,11 @@ function update_position(player, x, y){
         "Authorization": `JWT ${token}`
     };
     return fetch("http://104.237.1.145:11000/graphql/", {
-    // return fetch("http://localhost:11000/graphql/", {
-    "method": "POST",
-    "headers": headers,
-    "body": `{\"query\":\"mutation { updatePosition(input: { reference: \\\"${player}\\\" location: { x: ${x} y: ${y} } }){ entity { name location { x y } } } }\"}`
+        // TODO: remove commented code
+        // return fetch("http://localhost:11000/graphql/", {
+        "method": "POST",
+        "headers": headers,
+        "body": `{\"query\":\"mutation { updatePosition(input: { reference: \\\"${player}\\\" location: { x: ${x} y: ${y} } }){ entity { name location { x y } } } }\"}`
     })
     .then(json)
     .then(data => {
@@ -80,4 +106,39 @@ function update_position(player, x, y){
     .catch(err => {
             console.error(err);
     });
-}
+};
+
+
+function send_chat_message(player_name, message, chat_zone){
+    /*
+    Sends a chat message.
+    The chat_zone param on the backend is an enum, so the payload
+    of this parameter must be ALL CAPS and without quotation marks
+    around ir on the mutation input.
+        - Params:
+            + player_name: string;
+            + message: string;
+            + chat_zone: string (ALL CAPS);
+        - Return: null | undefined
+    */
+    var token = localStorage.getItem('token');
+    var headers = {
+        "cookie": "csrftoken=9YXcKsPnJSojmIXsjvqlM7TFP0tBfiU8GwVopYDWNKHSQnEUKLnPzJdsCjSb0Cfn",
+        "Content-Type": "application/json",
+        "Authorization": `JWT ${token}`
+    };
+    return fetch("http://104.237.1.145:11000/graphql/", {
+        // TODO: remove commented code
+        // return fetch("http://localhost:11000/graphql/", {
+        "method": "POST",
+        "headers": headers,
+        "body": `{\"query\":\"mutation { sendChatMessage(input: { playerName: \\\"${player_name}\\\" message: \\\"${message}\\\" chatZone: ${chat_zone.toUpperCase()} }){ chatMessage { message } } }\"}`
+    })
+    .then(json)
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+            console.error(err);
+    });
+};
